833e037f9b4468ce82588f99285e601529b3f410
Ulises Cervino Beresi <ulises@cloudant.com>
2013-07-08 21:17:41 +0100

    Deploy to clojars; add license information, etc.

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
-(defproject garlic-storm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+(defproject garlic-storm "0.0.1-SNAPSHOT"
+  :description "An implementation of graphite relays"
+
+  :url "http://github.com/ulises/garlic-storm"
+  :license {:name "Apache License Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            :distribution :repo}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :global-vars {*warn-on-reflection* true}
+
+  :deploy-branches ["master"]
+
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]]
+
+  :aot :all)
