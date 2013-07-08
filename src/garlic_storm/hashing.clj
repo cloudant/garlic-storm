3c08324068f9ecfed180ab3330407a496178985f
Ulises Cervino Beresi <ulises@cloudant.com>
2013-07-08 13:53:25 +0100

    Initial version of the consistent hashing thing. It comforms to graphite's in how it calculates a node's hash.

---
+++ src/garlic_storm/hashing.clj
@@ -0,0 +1,40 @@
+(ns garlic-storm.hashing
+  (:import (java.security MessageDigest)))
+
+;;; Taken mostly from
+;;; http://nakkaya.com/2010/05/05/consistent-hashing-with-clojure/
+;;; with the exception of the hash function which behaves like the one
+;;; used in
+;;; https://github.com/graphite-project/carbon/blob/master/lib/carbon/hashing.py#L16
+
+(defn- md5-hash
+  "Calculate hash for the given object. This implementation follows closely
+   that of graphite's compute_ring_position() in hashing.py"
+  [o]
+  (let [bytes (.getBytes (with-out-str (pr o)))
+        digest (.digest (MessageDigest/getInstance "MD5")
+                        (.getBytes o))
+        hexdigest (.toString (BigInteger. 1 digest) 16)]
+    (Integer/parseInt (reduce str (take 4 hexdigest)) 16)))
+
+(defn- hash-node [node replicas]
+  (map #(sorted-map (md5-hash (str node ":" %)) node) (range replicas)))
+
+(defn add-node [ring node replicas]
+  (apply merge ring (hash-node node replicas)))
+
+(defn remove-node [ring node replicas]
+  (apply dissoc ring (map first (map keys (hash-node node replicas)))))
+
+(defn make-ring [nodes replicas]
+  (reduce (fn [ring node] (add-node ring node replicas)) (sorted-map) nodes))
+
+(defn- tail-map [ring hash]
+  (filter #(< 0 (compare (key %) hash)) ring))
+
+(defn node-for [ring o]
+  (let [hash (md5-hash o)
+        tmap (tail-map ring hash)]
+    (if (empty? tmap)
+      (val (first ring))
+      (val (first tmap)))))
---
+++ test/garlic_storm/hashing_test.clj
@@ -0,0 +1,42 @@
+(ns garlic-storm.hashing-test
+  (:require [clojure.test :refer :all]
+            [garlic-storm.hashing :refer :all]))
+
+(deftest creating-rings
+  (testing "Creating a ring with replicas <= 0 results in empty ring"
+    (is (= {} (make-ring ["node1" "node2"] 0)))
+    (is (= {} (make-ring ["node1" "node2"] -5))))
+
+  (testing "Creating a ring with no nodes results in empty ring"
+    (is (= {} (make-ring [] 5)))
+    (is (= {} (make-ring nil 5))))
+
+  (testing "A ring is a sorted-map where k1 < k2 < ..."
+    (let [ring (make-ring ["foo" "bar"] 3)]
+      (loop [entry1 (first ring) entry2 (second ring) ring (drop 2 ring)]
+        (when (and entry1 entry2 ring)
+          (is (< (first entry1) (first entry2)))
+          (recur (first ring) (second ring) (drop 2 ring)))))))
+
+(deftest adding-removing-nodes
+  (testing "Removing a node returns a ring with no entries for it"
+    (let [replicas 3
+          ring (make-ring ["foo"] replicas)
+          ring-with-bar (add-node ring "bar" replicas)]
+      (is (= ring (remove-node ring-with-bar "bar" replicas)))))
+
+  (testing "Adding a node returns a ring with entries for it"
+    (let [replicas 3
+          foo-bar-ring (make-ring ["foo" "bar"] replicas)
+          foo-ring (make-ring ["foo"] replicas)]
+      (is (= foo-bar-ring (add-node foo-ring "bar" replicas))))))
+
+(deftest just-like-graphites-hashing
+  (testing "Should return the same node as graphite's hashing"
+    (let [ring (make-ring ["foo" "bar"] 5)]
+      (is (= "bar" (node-for ring "hi")))
+      (is (= "foo" (node-for ring "some.service.com")))))
+
+  (testing "Should be able to hash [service-name instance]"
+    (let [ring (make-ring [["service" 1] ["another-service" 1]] 5)]
+      (is (= ["another-service" 1] (node-for ring "foo"))))))
